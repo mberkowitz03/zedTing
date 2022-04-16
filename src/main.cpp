4b9ca96b1a3771c672916a2db4c0ef3e727fca0d
Aditya-Singhvi <singhvi@umich.edu>
2022-04-16 17:37:09 -0400

    pc compression

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,8 @@
 #include <pcl/common/common.h>
 #include "json.hpp"
 
-inline float convertColor(float colorIn) {
-    uint32_t color_uint = *(uint32_t *) & colorIn;
-    unsigned char *color_uchar = (unsigned char *) &color_uint;
-    color_uint = ((uint32_t) color_uchar[0] << 16 | (uint32_t) color_uchar[1] << 8 | (uint32_t) color_uchar[2]);
-    return *reinterpret_cast<float *> (&color_uint);
-}
-
-void get_pcl_cloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_cloud, sl::Camera &zed)
+void get_pcl_cloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_cloud, sl::Mat &zed_pc)
 {
-    sl::Mat zed_pc;
     zed.retrieveMeasure(zed_pc, sl::MEASURE::XYZRGBA, sl::MEM::GPU);
 
     float *p_zed_pc = zed_pc.getPtr<float>();
@@ -25,24 +17,15 @@ void get_pcl_cloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_cloud, sl::Came
     // Check and adjust points for PCL format
     for (auto &it : p_pcl_cloud->points) {
         float X = p_zed_pc[index];
-        if (!isValidMeasure(X)) // Checking if it's a valid point
-            it.x = it.y = it.z = it.rgb = 0;
-        else {
-            it.x = X;
-            it.y = p_zed_pc[index + 1];
-            it.z = p_zed_pc[index + 2];
-            it.rgb = convertColor(p_zed_pc[index + 3]); // Convert a 32bits float into a pcl .rgb format
-        }
+
+        it.x = X;
+        it.y = p_zed_pc[index + 1];
+        it.z = p_zed_pc[index + 2];
+        it.rgb = convertColor(p_zed_pc[index + 3]); // Convert a 32bits float into a pcl .rgb format
+
         index += 4;
     }
 }
-
-void write_binary_data(std::string fileName, pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud)
-{   
-    pcl::PCDWriter writer;
-    writer.writeBinaryCompressed(fileName, pcl_cloud); 
-}
-
 
 
 int main(int argc, char **argv) {
@@ -53,25 +36,38 @@ int main(int argc, char **argv) {
     // Set configuration parameters (@TODO: confirm)
     sl::InitParameters init_params;
     init_params.sdk_verbose = true; // Disable verbose mode
-    init_params.camera_resolution = sl::RESOLUTION::VGA;
+    init_params.camera_resolution = RESOLUTION::VGA;
     init_params.camera_fps = 30;
+    init_params.coordinate_units = UNIT::MILLIMETER;
+    init_params.depth_mode = DEPTH_MODE::ULTRA;
 
     // Open the camera
     sl::ERROR_CODE err = zed.open(init_params);
     if (err != sl::ERROR_CODE::SUCCESS) {
         exit(-1);
     }
+    
+    //Zed point cloud 
+    sl::Mat &zed_pc
 
     //PCL point cloud
+    sl::Resolution cloud_res(640, 360);
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr 
                 p_pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    p_pcl_point_cloud->points.resize(cloud_res.area());
+
+    // Gets ZED point cloud and converts to PCL point cloud
+    get_pcl_cloud(p_pcl_cloud, zed_pc); 
 
     //Data stream 
-    std::string fileName = "test.pcd";
+    std::ostringstream oss;
 
-    get_pcl_cloud(p_pcl_cloud, zed); 
-    write_binary_data(fileName, *p_pcl_cloud);
+    //PCL encoder / decoder
+    pcl::io::OctreePointCloudCompression<pcl::PointXYZRGBA>* PointCloudEncoder;
+    //pcl::io::OctreePointCloudCompression<pcl::PointXYZRGBA>* PointCloudDecoder;
 
+    PointCloudEncoder->encodePointCloud (p_pcl_cloud, oss);
+    
     // Close the camera
     zed.close();
     return 0;
