91dbd39fb0da92ffa6532655866bce15e617d4b3
Aditya-Singhvi <singhvi@umich.edu>
2022-04-16 17:10:07 -0400

    modularized some shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,31 @@
 #include <sl/Camera.hpp>
 #include <glm/vec4.hpp>
 
-struct GPU_Cloud {
-    sl::float4* data;
-    int size;
-};
+void get_pcl_cloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_cloud, sl::Mat &zed_pc)
+{
+    zed.retrieveMeasure(zed_pc, sl::MEASURE::XYZRGBA, sl::MEM::GPU);
 
-void getRawCloud(GPU_Cloud& pc, sl::Mat& zed_cloud) {
-    sl::float4* ptr = zed_cloud.getPtr<sl::float4>(sl::MEM::GPU);
-    pc.data = (sl::float4*) ptr;
-    pc.size = zed_cloud.getWidth() * zed_cloud.getHeight();
+    float *p_zed_pc = zed_pc.getPtr<float>();
+    int index = 0;
+
+    // Check and adjust points for PCL format
+    for (auto &it : p_pcl_cloud->points) {
+        float X = p_zed_pc[index];
+        if (!isValidMeasure(X)) // Checking if it's a valid point
+            it.x = it.y = it.z = it.rgb = 0;
+        else {
+            it.x = X;
+            it.y = p_zed_pc[index + 1];
+            it.z = p_zed_pc[index + 2];
+            it.rgb = convertColor(p_zed_pc[index + 3]); // Convert a 32bits float into a pcl .rgb format
+        }
+        index += 4;
+    }
 }
 
-void writeCloud(ofstream &fout, GPU_Cloud gpu_pc, int width, int height) 
-{
-    if (!fout.is_open()) cerr << "Could not open file!" << endl;
-
-    glm::vec4* pc = new glm::vec4[gpu_pc.size];
-    cudaMemcpy(pc, gpu_pc.data, sizeof(glm::vec4) * gpu_pc.size, cudaMemcpyDeviceToHost);
-
-    unsigned int rgba;
-    for (int i = 0; i < gpu_pc.size; ++i) {
-        const vec4& point = pc[i];
-        rgba = *reinterpret_cast<const unsigned int*>(&point.w);
-        rgba = (rgba & 0xFF) << 16 | (rgba & 0xFF00) | (rgba & 0xFF0000) >> 16;
-
-        fout << std::setprecision(8) << point.x << ' ' << point.y << ' ' << point.z << ' ' << rgba << '\n';
-    }
-
-    std::cout << "Wrote a point cloud of size " << width << " x " << height << '\n';
-
-    delete[] pc;
+void write_binary_data(std::ostream &os, pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud)
+{   
+    pcl::PCDWriter::writeBinaryCompressed(os, pcl_cloud); 
 }
 
 
@@ -39,39 +34,37 @@ int main(int argc, char **argv) {
     // Create a ZED camera object
     sl::Camera zed;
 
-    // Set configuration parameters
+    // Set configuration parameters (@TODO: confirm)
     sl::InitParameters init_params;
     init_params.sdk_verbose = true; // Disable verbose mode
+    init_params.camera_resolution = RESOLUTION::VGA;
+    init_params.camera_fps = 30;
+    init_params.coordinate_units = UNIT::MILLIMETER;
+    init_params.depth_mode = DEPTH_MODE::ULTRA;
 
     // Open the camera
     sl::ERROR_CODE err = zed.open(init_params);
     if (err != sl::ERROR_CODE::SUCCESS) {
         exit(-1);
     }
-    // Get camera information (ZED serial number)
-<<<<<<< HEAD
-    //int zed_serial = zed.getCameraInformation().serial_number;
-    //printf("Hello! This is my serial number: %d\n", zed_serial);
-=======
-    sl::Mat pcdMat;
-    GPU_Cloud gpuCloud;
-    zed.retrieveMeasure(pcdMat, sl::MEASURE::XYZRGBA, sl::MEM::GPU);
-    getRawCloud(gpuCloud, pcdMat);
->>>>>>> 6020464cadbb7463944b541c9c9ee9ed30c48e5e
+    
+    //Zed point cloud 
+    sl::Mat &zed_pc
 
-    // Get camera resolution
-    int width = 69;
-    int height = 69; 
+    //PCL point cloud
+    sl::Resolution cloud_res(640, 360);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr 
+                p_pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    p_pcl_point_cloud->points.resize(cloud_res.area());
 
-    sl::Mat pcdMat;
-    GPU_Cloud gpu_pc;
-    zed.retrieveMeasure(pcdMat, sl::MEASURE::XYZRGBA, sl::MEM::GPU);
-    getRawCloud(gpu_pc, frame);
+    //Data stream 
+    std::ostringstream oss;
 
-    ofstream fout("test.pcd");
-    writeCloud(fout, gpu_pc, width, height); 
+    get_pcl_cloud(p_pcl_cloud, zed_pc); 
+    write_binary_data(oss, *p_pcl_cloud);
 
     // Close the camera
     zed.close();
     return 0;
 }
+
