1075c0d3d032975a706f9ac04851289ee3eb4b06
Max Berkowitz <maxberk@umich.edu>
2022-04-16 17:01:11 -0400

    pcl imports and copy pasted stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,21 @@
 #include <sl/Camera.hpp>
 #include <glm/vec4.hpp>
+#include <iostream>
+#include <fstream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/common.h>
+#include "json.hpp"
 
-struct GPU_Cloud {
-    sl::float4* data;
-    int size;
-};
-
-void getRawCloud(GPU_Cloud& pc, sl::Mat& zed_cloud) {
-    sl::float4* ptr = zed_cloud.getPtr<sl::float4>(sl::MEM::GPU);
-    pc.data = (sl::float4*) ptr;
-    pc.size = zed_cloud.getWidth() * zed_cloud.getHeight();
-}
-
-void writeCloud(ofstream &fout, GPU_Cloud gpu_pc, int width, int height) 
-{
-    if (!fout.is_open()) cerr << "Could not open file!" << endl;
+void writeCloud(std::ofstream &fout, GPU_Cloud gpu_pc, int width, int height) {
+    if (!fout.is_open()) std::cerr << "Could not open file!" << std::endl;
 
     glm::vec4* pc = new glm::vec4[gpu_pc.size];
     cudaMemcpy(pc, gpu_pc.data, sizeof(glm::vec4) * gpu_pc.size, cudaMemcpyDeviceToHost);
 
     unsigned int rgba;
     for (int i = 0; i < gpu_pc.size; ++i) {
-        const vec4& point = pc[i];
+        const glm::vec4& point = pc[i];
         rgba = *reinterpret_cast<const unsigned int*>(&point.w);
         rgba = (rgba & 0xFF) << 16 | (rgba & 0xFF00) | (rgba & 0xFF0000) >> 16;
 
@@ -32,6 +26,7 @@ void writeCloud(ofstream &fout, GPU_Cloud gpu_pc, int width, int height)
 
     delete[] pc;
 }
+
 
 
 int main(int argc, char **argv) {
@@ -48,28 +43,43 @@ int main(int argc, char **argv) {
     if (err != sl::ERROR_CODE::SUCCESS) {
         exit(-1);
     }
+
+    sl::Resolution cloud_res = zed.getCameraInformation().camera_configuration.resolution;
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr p_pcl_point_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    p_pcl_point_cloud->points.resize(cloud_res.area());
+    
+
     // Get camera information (ZED serial number)
-<<<<<<< HEAD
-    //int zed_serial = zed.getCameraInformation().serial_number;
-    //printf("Hello! This is my serial number: %d\n", zed_serial);
-=======
     sl::Mat pcdMat;
-    GPU_Cloud gpuCloud;
     zed.retrieveMeasure(pcdMat, sl::MEASURE::XYZRGBA, sl::MEM::GPU);
-    getRawCloud(gpuCloud, pcdMat);
->>>>>>> 6020464cadbb7463944b541c9c9ee9ed30c48e5e
+
+    float *p_data_cloud = pcdMat.getPtr<float>();
+    int index = 0;
+
+    // Check and adjust points for PCL format
+    for (auto &it : p_pcl_point_cloud->points) {
+        float X = p_data_cloud[index];
+        if (!isValidMeasure(X)) // Checking if it's a valid point
+            it.x = it.y = it.z = it.rgb = 0;
+        else {
+            it.x = X;
+            it.y = p_data_cloud[index + 1];
+            it.z = p_data_cloud[index + 2];
+            it.rgb = convertColor(p_data_cloud[index + 3]); // Convert a 32bits float into a pcl .rgb format
+        }
+        index += 4;
+    }
+
+    pcl::PCDWriter writer;
+    writer.writeBinaryCompressed();
 
     // Get camera resolution
     int width = 69;
     int height = 69; 
 
-    sl::Mat pcdMat;
-    GPU_Cloud gpu_pc;
-    zed.retrieveMeasure(pcdMat, sl::MEASURE::XYZRGBA, sl::MEM::GPU);
-    getRawCloud(gpu_pc, frame);
-
-    ofstream fout("test.pcd");
-    writeCloud(fout, gpu_pc, width, height); 
+    std::ofstream fout("test.pcd");
+    writeCloud(fout, gpuCloud, width, height); 
 
     // Close the camera
     zed.close();
