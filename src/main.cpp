1b89f20eeac71e8c50dd24f051b5c8324938e462
Max Berkowitz <maxberk@umich.edu>
2022-04-16 16:09:13 -0400

    gpu_cloud shtuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,40 @@
 #include <sl/Camera.hpp>
-using namespace sl;
+#include <glm/vec4.hpp>
+
+struct GPU_Cloud {
+    sl::float4* data;
+    int size;
+};
+
+void getRawCloud(GPU_Cloud& pc, sl::Mat& zed_cloud) {
+    sl::float4* ptr = zed_cloud.getPtr<sl::float4>(sl::MEM::GPU);
+    pc.data = (sl::float4*) ptr;
+    pc.size = zed_cloud.getWidth() * zed_cloud.getHeight();
+}
 
 int main(int argc, char **argv) {
 
     // Create a ZED camera object
-    Camera zed;
+    sl::Camera zed;
 
     // Set configuration parameters
-    InitParameters init_params;
-    init_params.sdk_verbose = false; // Disable verbose mode
+    sl::InitParameters init_params;
+    init_params.sdk_verbose = true; // Disable verbose mode
+
 
     // Open the camera
-    ERROR_CODE err = zed.open(init_params);
-    if (err != ERROR_CODE::SUCCESS)
+    sl::ERROR_CODE err = zed.open(init_params);
+    if (err != sl::ERROR_CODE::SUCCESS) {
         exit(-1);
-
+    }
     // Get camera information (ZED serial number)
-    int zed_serial = zed.getCameraInformation().serial_number;
+    sl::Mat pcdMat;
+    zed.retrieveMeasure(pcdMat, sl::MEASURE::XYZRGBA, sl::MEM::GPU);
 
-    printf("Hello! This is my serial number: %d\n", zed_serial);
+
+
+    //int zed_serial = zed.getCameraInformation().serial_number;
+    //printf("Hello! This is my serial number: %d\n", zed_serial);
 
     // Close the camera
     zed.close();
